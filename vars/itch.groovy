eb905663d1b3ae0ec9b9e5b533300f0e4c707541
timrademaker <10331650+timrademaker@users.noreply.github.com>
2020-10-07 12:03:57 +0200

    Made class members static because I can't get global variables to work

--- vars/itch.groovy
+++ vars/itch.groovy
@@ -1,20 +1,16 @@
 class ItchConfiguration {
-    String butlerExe = '';
-    String user = '';
-    String game = '';
-    String channel = '';
-    String apiKeyID = '';
-    List<String> ignoredFiles = [];
+    static String butlerExe = '';
+    static String user = '';
+    static String apiKeyID = '';
 }
 
-def itchConfig = new ItchConfiguration();
 
 def init(String butlerExecutable, String apiKeyID = '', String username = '') {
     assert(file.exists(butlerExecutable));
 
-    itchConfig.butlerExe = butlerExecutable;
-    itchConfig.apiKeyID = apiKeyID;
-    itchConfig.user = username;
+    ItchConfiguration.butlerExe = butlerExecutable;
+    ItchConfiguration.apiKeyID = apiKeyID;
+    ItchConfiguration.user = username;
 }
 
 def push(String gameDirectory, String gameName, String channel, Boolean dryRun = false, List<String> ignoredFiles = []) {
@@ -32,9 +28,9 @@ def push(String gameDirectory, String gameName, String channel, Boolean dryRun =
         dryRunStr = '--dry-run';
     }
 
-    withCredentials([string(credentialsId: itchConfig.apiKeyID, variable: 'BUTLER_API_KEY')]) {
+    withCredentials([string(credentialsId: ItchConfiguration.apiKeyID, variable: 'BUTLER_API_KEY')]) {
         bat label: 'Upload build to itch.io', script: """${config.butlerExe} login
-                        ${itchConfig.butlerExe} push --if-changed --assume-yes ${ignoreStr} ${dryRunStr} '${gameDirectory}' '${itchConfig.username}/${gameName}:${channel}'
-                        ${itchConfig.butlerExe} logout --assume-yes"""
+                        ${ItchConfiguration.butlerExe} push --if-changed --assume-yes ${ignoreStr} ${dryRunStr} '${gameDirectory}' '${ItchConfiguration.username}/${gameName}:${channel}'
+                        ${ItchConfiguration.butlerExe} logout --assume-yes"""
     }
 }
--- vars/ue4.groovy
+++ vars/ue4.groovy
@@ -1,23 +1,21 @@
-class ueConfiguration {
-    String engineRootDirectory = '';
-    String targetPlatform = 'Win64';
-    String buildConfiguration = 'Development';
+class UnrealConfiguration {
+    static String engineRootDirectory = '';
+    static String targetPlatform = 'Win64';
+    static String buildConfiguration = 'Development';
 }
-
-def ueConfig = new ueConfiguration();
 
 
 def init(String ueDir, String platform = '', String configuration = '') {
     assert(file.dirExists(ueDir));
 
-    ueConfig.ueRootDirectory = ueDir;
+    UnrealConfiguration.engineRootDirectory = ueDir;
 
     if(platform) {
-        ueConfig.targetPlatform = platform;
+        UnrealConfiguration.targetPlatform = platform;
     }
 
     if(configuration) {
-        ueConfig.buildConfiguration = configuration;
+        UnrealConfiguration.buildConfiguration = configuration;
     }
 }
 
@@ -27,21 +25,21 @@ def build(String projectDir, String projectName, String outputDirectory, String 
     assert(outputDirectory);
 
     if(!platform) {
-        platform = ueConfig.targetPlatform;
+        platform = UnrealConfiguration.targetPlatform;
     }
 
     if(!configuration) {
-        configuration = ueConfig.configuration;
+        configuration = UnrealConfiguration.configuration;
     }
 
     if(!logFile) {
         logFile = 'build.log';
     }
     
-    bat label: 'Generate Project Files', script: "CALL \"${ueConfig.engineRootDirectory}\\Engine\\Binaries\\DotNET\\UnrealBuildTool.exe\" -projectfiles -project=\"${projectDir}\\${projectName}.uproject\" -game -rocket -progress -nointellisense -WaitMutex -Platforms=\"${platform}\" -log=\"${logFile}\" PrecompileForTargets = PrecompileTargetsType.Any;"
+    bat label: 'Generate Project Files', script: "CALL \"${UnrealConfiguration.engineRootDirectory}\\Engine\\Binaries\\DotNET\\UnrealBuildTool.exe\" -projectfiles -project=\"${projectDir}\\${projectName}.uproject\" -game -rocket -progress -nointellisense -WaitMutex -Platforms=\"${platform}\" -log=\"${logFile}\" PrecompileForTargets = PrecompileTargetsType.Any;"
 
     if(configuration == 'Development') {
-        bat label: 'Build binaries', script: "CALL \"${ueConfig.engineRootDirectory}\\Engine\\Build\\BatchFiles\\Build.bat\" \"${projectName}Editor\" \"${projectDir}\\${projectName}.uproject\" ${platform} ${configuration} -log=\"${logFile}\""
+        bat label: 'Build binaries', script: "CALL \"${UnrealConfiguration.engineRootDirectory}\\Engine\\Build\\BatchFiles\\Build.bat\" \"${projectName}Editor\" \"${projectDir}\\${projectName}.uproject\" ${platform} ${configuration} -log=\"${logFile}\""
     }
 
 }
@@ -52,16 +50,16 @@ def packageBuild(String projectDir, String projectName, String outputDirectory, 
     assert(outputDirectory);
     
     if(!platform) {
-        platform = ueConfig.targetPlatform;
+        platform = UnrealConfiguration.targetPlatform;
     }
 
     if(!configuration) {
-        configuration = ueConfig.configuration;
+        configuration = UnrealConfiguration.configuration;
     }
 
     if(!logFile) {
         logFile = 'package.log';
     }
 
-    bat label: 'Package project', script: "CALL \"${ueConfig.engineRootDirectory}\\Engine\\Build\\BatchFiles\\RunUAT.bat\" BuildCookRun -project=\"${projectDir}\\${projectName}.uproject\" -noP4 -Distribution -Cook -Build -Stage -Pak -Rocket -Prereqs -Package -TargetPlatform=${platform} -Platform=${platform} -ClientConfig=${configuration} -ServerConfig=${configuration} -Archive -archivedirectory=\"${outputDirectory}\" -log=\"${logFile}\""
+    bat label: 'Package project', script: "CALL \"${UnrealConfiguration.engineRootDirectory}\\Engine\\Build\\BatchFiles\\RunUAT.bat\" BuildCookRun -project=\"${projectDir}\\${projectName}.uproject\" -noP4 -Distribution -Cook -Build -Stage -Pak -Rocket -Prereqs -Package -TargetPlatform=${platform} -Platform=${platform} -ClientConfig=${configuration} -ServerConfig=${configuration} -Archive -archivedirectory=\"${outputDirectory}\" -log=\"${logFile}\""
 }
