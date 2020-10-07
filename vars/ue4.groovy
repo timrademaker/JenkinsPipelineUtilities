e66d1d3a537bd056096e4c66eb30f8dab54a24fd
timrademaker <10331650+timrademaker@users.noreply.github.com>
2020-10-07 10:51:14 +0200

    Implemented package

--- vars/ue4.groovy
+++ vars/ue4.groovy
@@ -12,8 +12,8 @@ class ueConfig {
 def config = new ueConfig();
 
 
-def init(String ueDir, String projectDir, String projectName, String outputDirectory, String platform = config.targetPlatform, String configuration = config.buildConfiguration, String logFile = config.logFile) {
-    // TODO: Ensure dirs are valid
+def init(String ueDir, String projectDir = config.projectDirectory, String projectName = config.projectName, String outputDirectory = config.buildOutDirectory, String platform = config.targetPlatform, String configuration = config.buildConfiguration, String logFile = config.logFile) {
+    assert(file.dirExists(ueDir));
 
     config.ueRootDirectory = ueDir;
     config.projectDirectory = projectDir;
@@ -24,22 +24,27 @@ def init(String ueDir, String projectDir, String projectName, String outputDirec
     config.buildOutDirectory = outputDirectory;
 }
 
-def build(String ueDir = config.engineRootDirectory, String projectDir = config.projectDirectory, String projectName = config.projectName, String outputDirectory = config.buildOutDirectory, String platform = config.targetPlatform, String configuration = config.buildConfiguration, String logFile = config.logFile) {
-    assert(ueDir);
-    assert(projectDir);
-    assert(projectName);
+def build(String projectDir = config.projectDirectory, String projectName = config.projectName, String outputDirectory = config.buildOutDirectory, String platform = config.targetPlatform, String configuration = config.buildConfiguration, String logFile = config.logFile) {
+    assert(file.dirExists(projectDir));
+    assert(file.exists("${projectDir}/${projectName}.uproject"));
     assert(outputDirectory);
     assert(platform);
     assert(configuration);
     
-    bat label: 'Generate Project Files', script: "CALL \"${ueDir}\\Engine\\Binaries\\DotNET\\UnrealBuildTool.exe\" -projectfiles -project=\"${projectDir}\\${projectName}.uproject\" -game -rocket -progress -2019 -nointellisense -Platforms=\"${platform}\" -log=\"${logFile}\" PrecompileForTargets = PrecompileTargetsType.Any;"
+    bat label: 'Generate Project Files', script: "CALL \"${config.engineRootDirectory}\\Engine\\Binaries\\DotNET\\UnrealBuildTool.exe\" -projectfiles -project=\"${projectDir}\\${projectName}.uproject\" -game -rocket -progress -nointellisense -WaitMutex -Platforms=\"${platform}\" -log=\"${logFile}\" PrecompileForTargets = PrecompileTargetsType.Any;"
 
     if(config.buildConfiguration == 'Development') {
-        bat label: 'Build binaries', script: "CALL \"${ueDir}\\Engine\\Build\\BatchFiles\\Build.bat\" \"${projectName}Editor\" \"${projectDir}\\${projectName}.uproject\" ${platform} ${configuration} -log=\"${logFile}\""
+        bat label: 'Build binaries', script: "CALL \"${config.engineRootDirectory}\\Engine\\Build\\BatchFiles\\Build.bat\" \"${projectName}Editor\" \"${projectDir}\\${projectName}.uproject\" ${platform} ${configuration} -log=\"${logFile}\""
     }
 
 }
 
-def package() {
+def package(String ueDir = config.engineRootDirectory, String projectDir = config.projectDirectory, String projectName = config.projectName, String outputDirectory = config.buildOutDirectory, String platform = config.targetPlatform, String configuration = config.buildConfiguration, String logFile = config.logFile) {
+    assert(file.dirExists(projectDir));
+    assert(file.exists("${projectDir}/${projectName}.uproject"));
+    assert(outputDirectory);
+    assert(platform);
+    assert(configuration);
 
+    bat label: 'Package project', script: "CALL \"${config.engineRootDirectory}\\Engine\\Build\\BatchFiles\\RunUAT.bat\" BuildCookRun -project=\"${projectDir}\\${projectName}.uproject\" -noP4 -Distribution -Cook -Build -Stage -Pak -Rocket -Prereqs -Package -TargetPlatform=${platform} -Platform=${platform} -ClientConfig=${configuration} -ServerConfig=${configuration} -Archive -archivedirectory=\"${outputDirectory}\" -log=\"${logFile}\""
 }
